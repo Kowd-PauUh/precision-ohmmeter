f353428f048c32cfeb81ca21b9c0f703cec7e8e9
Ivan <kowd.pauuh@gmail.com>
2025-06-24 16:12:21 +0200

    Updated resistance computation with regard to diff. amp. gain

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -21,6 +21,11 @@ limitations under the License.
 constexpr float current = 0.005;                                          // 5 mA
 constexpr uint8_t adc_bits = 16;                                          // 16-bit ADC
 constexpr float adc_max_voltage = 3.3;                                    // 3.3 V
+constexpr float R4 = 10'000.0f;                                           // diff. amp. resistors
+constexpr float R5 = 330'000.0f;
+constexpr float R8 = 1'000.0f;
+constexpr float mode_0_gain = R5 * (R4 + R8) / (R4 * R8);                 // diff. amp. gain
+constexpr float mode_1_gain = R5 / R4;
 // U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);   // LCD on 1st I2C interface (optional)
 U8G2_SSD1306_128X32_UNIVISION_F_2ND_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);  // LCD on 2nd I2C interface
 
@@ -46,7 +51,7 @@ void displayText(const char* line1, const char* line2) {
     u8g2.sendBuffer();
 }
 
-float compute_resistance(uint32_t adc_reading, float current) {
+float compute_resistance(uint32_t adc_reading, float current, float diff_amp_gain) {
     // R = U / I
 
     if (current == 0.0f) {
@@ -54,13 +59,20 @@ float compute_resistance(uint32_t adc_reading, float current) {
     }
 
     float voltage_drop = adc_max_voltage * static_cast<float>(adc_reading) / static_cast<float>(adc_range);
+    voltage_drop /= diff_amp_gain;
     return voltage_drop / current;
 }
 
 void loop() {
-    resistance = compute_resistance(0, current);
+    // compute resistance value
+    resistance = compute_resistance(10000, current, mode_0_gain);
 
-    displayText("0.00 V", "0.0000 Ohm");
+    // format resistance for displaying
+    char resistance_str[15];
+    snprintf(resistance_str, sizeof(resistance_str), "%.5f Ohm", resistance);
+
+    // display data
+    displayText("0.00 V", resistance_str);
     delay(1000);
     displayText("", "");
     delay(1000);
