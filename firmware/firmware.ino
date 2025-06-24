9b70bafad33862bdd689718d42fb199f23b8e813
Ivan <kowd.pauuh@gmail.com>
2025-06-24 14:22:02 +0200

    Added LCD display configuration. Implemented displayText function

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -14,14 +14,39 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <U8g2lib.h>
+
 // modify these as you need
 uint8_t adc_bits = 16;
 
 // do not modify these
 uint32_t adc_range = 1 << adc_bits;
 
-void setup() {}
+// constructor for SSD1306 128x32, I2C, no reset pin
+// U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);  // LCD on 1st I2C interface
+U8G2_SSD1306_128X32_UNIVISION_F_2ND_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);  // LCD on 2nd I2C interface
+
+void setup() {
+    u8g2.begin();  // Initialize display
+}
+
+void displayText(const char* line1, const char* line2) {
+    u8g2.clearBuffer();
+
+    // 1st line, right-aligned
+    u8g2.setFont(u8g2_font_6x13_tf);
+    u8g2.drawStr(u8g2.getDisplayWidth() - u8g2.getStrWidth(line1), 12, line1);
+
+    // 2nd line, left-aligned
+    u8g2.setFont(u8g2_font_9x15B_tf);
+    u8g2.drawStr(0, 28, line2);
+
+    u8g2.sendBuffer();
+}
 
 void loop() {
-	// ...
+    displayText("0.00 V", "0.0000 Ohm");
+    delay(1000);
+    displayText("0.00 V", "0.0000 Ohm");
+    delay(1000);
 }
