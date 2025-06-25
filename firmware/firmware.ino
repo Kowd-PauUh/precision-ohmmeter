2610f5d0c876f84e2131d185d828d28bc6f4ceef
Ivan <kowd.pauuh@gmail.com>
2025-06-25 10:21:23 +0200

    Added voltage measurement display

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -21,15 +21,18 @@ limitations under the License.
 
 // ADJUST FOLLOWING AS YOU NEED
 
+// PCB
 constexpr float current = 0.005;                  // 5 mA
-constexpr uint8_t adc_bits = 16;                  // 16-bit ADC
-constexpr float adc_max_voltage = 3.3;            // 3.3 V
 constexpr float cell_voltage_divider_gain = 0.5;  // cell voltage divider gain before ADC
 
 // diff. amp. resistors
-constexpr float R4 = 10'000.0f;         // 10 kOhm
-constexpr float R5 = 330'000.0f;        // 330 kOhm
-constexpr float R8 = 1'000.0f;          // 1 kOhm
+constexpr float R4 = 10'000.0f;                   // 10 kOhm
+constexpr float R5 = 330'000.0f;                  // 330 kOhm
+constexpr float R8 = 1'000.0f;                    // 1 kOhm
+
+// ADC pinout
+constexpr uint8_t voltage_adc_pin = 0;
+constexpr uint8_t cell_voltage_adc_pin = 1;
 
 // LCD display
 // U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);   // LCD on 1st I2C interface
@@ -41,15 +44,15 @@ DFRobot_ADS1115 ads(&Wire);  // ADC on 1st I2C interface
 
 // DO NOT MODIFY THESE
 float cell_voltage, voltage, resistance;
-constexpr uint32_t adc_range = 1 << adc_bits;
 constexpr float mode_0_gain = R5 * (R4 + R8) / (R4 * R8);  // diff. amp. gain (~363) in first mode
 constexpr float mode_1_gain = R5 / R4;                     // diff. amp. gain (33) in second mode
 
 void setup() {
-    u8g2.begin();  // Initialize display
+    // initialize display
+    u8g2.begin();
     pinMode(29, OUTPUT);
 
-    Serial.begin(9600);
+    // initialize ADC
     ads.setAddr_ADS1115(ADS1115_IIC_ADDRESS0);   // 0x48
     ads.setGain(eGAIN_ONE);                      // no gain
     ads.setMode(eMODE_SINGLE);                   // single-shot mode
@@ -72,16 +75,14 @@ void displayText(const char* line1, const char* line2) {
     u8g2.sendBuffer();
 }
 
-float compute_resistance(uint32_t adc_reading, float current, float diff_amp_gain) {
+float compute_resistance(float voltage, float current, float diff_amp_gain) {
     // R = U / I
 
     if (current == 0.0f) {
         return INFINITY;
     }
 
-    float voltage_drop = adc_max_voltage * static_cast<float>(adc_reading) / static_cast<float>(adc_range);
-    voltage_drop /= diff_amp_gain;
-    return voltage_drop / current;
+    return voltage / diff_amp_gain / current;
 }
 
 float readVoltage(uint8_t channel) {
@@ -95,8 +96,14 @@ float readVoltage(uint8_t channel) {
 }
 
 void loop() {
+    digitalWrite(29, HIGH);
+
+    // read voltage from ADC
+    voltage = readVoltage(voltage_adc_pin);
+    cell_voltage = readVoltage(cell_voltage_adc_pin) / cell_voltage_divider_gain;
+
     // compute resistance value
-    resistance = compute_resistance(10000, current, mode_0_gain);
+    resistance = compute_resistance(voltage, current, mode_0_gain);
 
     // format resistance for displaying
     char resistance_str[15];
@@ -110,16 +117,5 @@ void loop() {
     displayText(cell_voltage_str, resistance_str);
     delay(1000);
     displayText("", "");
-    delay(1000);
-
-    digitalWrite(29, HIGH);
-    voltage = readVoltage(0);
-    Serial.print(voltage, 6);
-    Serial.println(" V");
-    delay(1000);
-    digitalWrite(29, LOW);
-    voltage = readVoltage(0);
-    Serial.print(voltage, 6);
-    Serial.println(" V");
     delay(1000);
 }
